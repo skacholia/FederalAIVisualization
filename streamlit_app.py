d151cd71eb55da3cf506c015e80437dc21b10f75
Suhan Kacholia <suhan@righttrackanalytics.org>
2024-02-06 12:02:56 -0700

    department

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,21 +8,21 @@ st.set_page_config(page_title='AI Implementation Stages and Techniques by Agency
 df = pd.read_csv('federalai.csv')  # Update this path
 
 # Sidebar for agency selection, adding an "Overall" option
-agency_list = ['Overall'] + list(df['Agency'].unique())
-selected_agency = st.sidebar.selectbox('Select an Agency:', agency_list)
+department_list = ['Overall'] + list(df['Department'].unique())
+selected_department = st.sidebar.selectbox('Select a Department:', department_list)
 
 # Adjust the dataset filtering
-if selected_agency == 'Overall':
+if selected_department == 'Overall':
     filtered_df = df  # Use the entire dataset for the "Overall" option
 else:
-    filtered_df = df[df['Agency'] == selected_agency]  # Filter by the selected agency
+    filtered_df = df[df['Department'] == selected_department]  # Filter by the selected agency
 
 # Count the occurrences of each development stage for the selected dataset
 stage_counts = filtered_df['Development_Stage'].value_counts().reset_index()
 stage_counts.columns = ['Stage', 'Count']
 
 # Create the pie chart for the selected dataset
-title_text = 'Stages of Implementation - Overall' if selected_agency == 'Overall' else f'Stages of Implementation for {selected_agency}'
+title_text = 'Stages of Implementation - Overall' if selected_department == 'Overall' else f'Stages of Implementation for {selected_agency}'
 pie_fig = px.pie(stage_counts, names='Stage', values='Count', title=title_text)
 
 # Count and plot the occurrences of techniques
